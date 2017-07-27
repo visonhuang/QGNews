44c6532b200a778764eb4f86f2c05da72cc749f9
jim ji <1035363309@qq.com>
2017-07-27 21:04:42 +0800

    加图片

--- app/src/main/java/com/qg/qgnews/ui/activity/MainActivity.java
+++ app/src/main/java/com/qg/qgnews/ui/activity/MainActivity.java
@@ -1,27 +1,72 @@
 package com.qg.qgnews.ui.activity;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.qg.qgnews.R;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
+    private Toolbar toolbar;
+    private SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
     }
-    private void initView(){
+
+    private void initView() {
         toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_toolbar_menu, menu);
+        searchView = ((SearchView) menu.findItem(R.id.main_menu_search).getActionView());
+        setSearchViewOptions(searchView);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_menu_search:
+                break;
+            case R.id.main_menu_logout:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setSearchViewOptions(final SearchView s) {
+        s.setQueryHint("搜索新闻...");
+        s.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("=========",newText);
+                if (newText.length() > 10) {
+                    s.setQuery(newText.substring(0,10),false);
+                }
+                return false;
+            }
+        });
     }
 }
