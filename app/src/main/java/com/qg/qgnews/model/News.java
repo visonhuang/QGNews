39875407beca6cf61f89da96fada375cecdbbd9a
jim ji <1035363309@qq.com>
2017-07-27 15:38:56 +0800

    分包

---
+++ app/src/main/java/com/qg/qgnews/model/News.java
@@ -0,0 +1,8 @@
+package com.qg.qgnews.model;
+
+/**
+ * Created by 小吉哥哥 on 2017/7/27.
+ */
+
+public class News {
+}
---
+++ app/src/main/java/com/qg/qgnews/util/Request.java
@@ -0,0 +1,8 @@
+package com.qg.qgnews.util;
+
+/**
+ * Created by 小吉哥哥 on 2017/7/27.
+ */
+
+public class Request {
+}
