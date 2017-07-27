eb2d8651d1625c5da10a8056977191887ebc0882
jim ji <1035363309@qq.com>
2017-07-27 16:25:36 +0800

    加图片

--- app/src/main/java/com/qg/qgnews/ui/activity/MainActivity.java
+++ app/src/main/java/com/qg/qgnews/ui/activity/MainActivity.java
@@ -2,14 +2,26 @@ package com.qg.qgnews.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.qg.qgnews.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    private void initView(){
+        toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
