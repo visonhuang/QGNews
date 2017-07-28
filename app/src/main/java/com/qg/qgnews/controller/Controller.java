47eac88d9c158f3c78316c0e013493cd0c08d170
littlejigege <1035363309@qq.com>
2017-07-28 17:05:22 +0800

    Update Controller.java

--- app/src/main/java/com/qg/qgnews/controller/Controller.java
+++ app/src/main/java/com/qg/qgnews/controller/Controller.java
@@ -6,7 +6,7 @@ import java.util.Collections;
  * Created by 小吉哥哥 on 2017/7/27.
  */
 
-public class Controller {
+public class controller {
     private static Controller sController = null;
 
     public Controller() {
